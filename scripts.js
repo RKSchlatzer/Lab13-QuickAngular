9e6b24f64c82554fc2e285d70382686cbd8614d5
RKSchlatzer <rkschlatzer@gmail.com>
2017-03-17 11:34:40 -0400

    Styled Divs, Added Floats
    Modified Div styling elements, Floated Nav in-line

--- scripts.js
+++ scripts.js
@@ -3,21 +3,41 @@ var app = angular.module("redditApp", []);
 app.controller("mainCtrl", function($scope) {
 
     $scope.mainContent = [{
+            image: "Images/snoo.png",
             title: "Title 1 - Lorem Title Lorem Title",
             description: "I'm a picture of a pretty animal. Blah blah blah. Upvote me."
         },
 
         {
+            image: "Images/snoo.png",
             title: "Title 2 - Lorem Title Lorem Title",
             description: "I'm another image of something. Blah blah blah. Upvote me."
         },
 
         {
+            image: "Images/snoo.png",
             title: "Title 3 - Lorem Title Lorem Title",
             description: "Probably a puppy doing something silly. Blah blah blah. Upvote me."
         },
+        {
+            image: "Images/snoo.png",
+            title: "Title 4 - Lorem Title Lorem Title",
+            description: "Cat falling off something. Blah blah blah. Upvote me."
+        },
+        {
+            image: "Images/snoo.png",
+            title: "Title 5 - Lorem Title Lorem Title",
+            description: "A crazy-weird iguana type lizard. Not a regular pet-type-lizard. Blah blah blah. Upvote me."
+        },
+        {
+            image: "Images/snoo.png",
+            title: "Title 6 - Lorem Title Lorem Title",
+            description: "Here is a kid doing something really cool. Blah blah blah. Upvote me."
+        },
 
     ];
+
+
 
 });
 
@@ -41,12 +61,12 @@ app.directive("titleBar", function() {
 
 });
 
-app.directive("sideNav", function(){
+app.directive("sideNav", function() {
 
-      return {
-          restrict: "E",
-          templateUrl: "side-nav.html",
-          replace: false
-      };
+    return {
+        restrict: "E",
+        templateUrl: "side-nav.html",
+        replace: false
+    };
 
 });
